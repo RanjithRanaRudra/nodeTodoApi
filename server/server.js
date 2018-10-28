241bba7bc5d9b0070b8855bcd0ae45fc3444c08e
RanjithRanaRudra <srisasanka096@gmail.com>
2018-10-28 12:32:21 +0530

    committed 28-10-2018 12:32

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.delete('/todos/:id', (req, res)=> {
         res.status(404).send();
     }
     // Delete the record by id
-    Todo.findByIdAndRemove(id).then((todos)=>{
+    Todo.findByIdAndDelete(id).then((todos)=>{
         if(!todos) {
             res.status(404).send();
         }
