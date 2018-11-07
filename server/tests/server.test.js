7aab9e976803c820348ed86e5a3c86514cab4db5
RanjithRanaRudra <srisasanka096@gmail.com>
2018-11-08 00:50:50 +0530

    committed on 08-11-2018 00:50

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 const assert = require('assert');
 
 const {Todo} = require('../models/todo');
+const {User} = require('../models/user');
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 
@@ -174,7 +175,7 @@ describe('Server', () => {
         });
     });
 
-    describe('#POST/users/me', () => {
+    describe('#GET/users/me', () => {
         it('should return user if authenticated', (done) => {
             request(app)
             .get('/users/me')
@@ -193,6 +194,52 @@ describe('Server', () => {
             .expect((res)=>{
                 assert.deepStrictEqual(res.body, {});
             })
+            .end(done);
+        });
+    });
+
+    describe('#POST/users', () => {
+        it('should create user', (done) => {
+            var email = 'abc@abc.com';
+            var password ='abc123';
+            request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.header['x-auth']).toBeDefined();
+                expect(res.body._id).toBeDefined();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err)=>{
+                if(err) {
+                    return done(err);
+                }
+
+                User.findOne({email}).then((user)=>{
+                    expect(user).toBeDefined();
+                    expect(user.password).not.toBe(password);
+                    done();
+                });
+
+            });
+        });
+        it('should return validation errors if request invalid', (done) => {
+            var email='and';
+            var password = 'pass123';
+            request(app)
+            .post('/users')
+            .send({email,password})
+            .expect(400)
+            .end(done);
+        });
+        it('should not create user if email exists', (done) => {
+            var email=users[0].email;
+            var password = 'pass123';
+            request(app)
+            .post('/users')
+            .send({email,password})
+            .expect(400)
             .end(done);
         });
     });
